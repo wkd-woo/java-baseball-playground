5efaed855f516ad62e5e4010f9997974f265e84b
wkd-woo <wkdwoos@gmail.com>
2021-06-25 20:10:33 +0900

    0 학습테스트 실습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
@@ -10,4 +12,41 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    /**
+     * 요구사항 1
+     */
+    @Test
+    @DisplayName("separating string bt semicolon")
+    void separate(){
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly("1","2");
+
+        String[] actual2 = "1".split(",");
+        assertThat(actual2).containsExactly("1");
+    }
+
+
+    /**
+     * 요구사항 2
+     */
+    @Test
+    @DisplayName("eliminating parenthesis")
+    void eliminate(){
+        String actual = "(1,2)".substring(1,4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    /**
+     * 요구사항 3
+     */
+    @Test
+    @DisplayName("Selecting specific letter")
+    void select(){
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class).isThrownBy(() -> {
+            char actual = "abc".charAt(4);
+            assertThat(actual).isEqualTo('b');
+        }).withMessageContaining("String index out of range");
+    }
+
 }
